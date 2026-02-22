aef9af5b244741c2b9f647df017822d5d335f4c4
Andrew Wells <andrewwe98@gmail.com>
2026-02-22 10:26:01 -0600

    change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pokeapi/',
+  base: process.env.NODE_ENV === 'production' ? '/Pokeapi/' : '/',
   plugins: [react()],
   
 })
